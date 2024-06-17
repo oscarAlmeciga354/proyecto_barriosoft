67a2573d9b03e4ac71f497b22ae292d56af810d0
oscarAlmeciga354 <oscar_almeciga@soy.sena.edu.co>
2024-06-16 19:28:07 -0500

    n

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers;
 
 Route::get('/', function () {
-    return view('inicio');
-});
-Route::get('login', function () {
-    return view('login');
-});
-Route::get('register', function () {
-    return view('registro');
-});
-Route::get('domiciliario', function () {
-    return view('domiciliario');
+    return view('welcome');
 });
 
-?>
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
